730733b56c98a0f049175ec5c16a8168f40d7754
gustavo-ataide <gugaben903@gmail.com>
2023-11-21 20:43:42 -0300

    Starting my own memoryze game

---
+++ MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Gustavo  Henrique on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
