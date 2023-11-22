4431508ba6acd4873a0862a2c13685012184dd8b
gustavo-ataide <gugaben903@gmail.com>
2023-11-22 12:20:16 -0300

    Now, Cards can be toggled.

--- MemoryGame/MemoryGame/ContentView.swift
+++ MemoryGame/MemoryGame/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(IsFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.tint)
         .padding()
+    }
+}
+struct CardView: View{
+    @State var IsFaceUp:Bool = false
+    let texto = "Just works"
+    var body: some View{
+        
+        let base  = RoundedRectangle(cornerRadius: 12)
+        ZStack {
+            if IsFaceUp{
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                VStack{
+                    Text("👨🏼‍💻").font(.largeTitle)
+                    Text(texto).font(.caption)
+                }
+                
+            }else{
+                base.fill()
+            }
+        }.onTapGesture {
+            IsFaceUp.toggle()
+        }
+        
     }
 }
 
