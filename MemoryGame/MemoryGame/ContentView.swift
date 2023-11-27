48ed2468dcb137222020bc7e66eef52df26ff29d
gustavo-ataide <gugaben903@gmail.com>
2023-11-27 16:29:50 -0300

    optimizing the code/logic, and fixing aspect ratio issues from Cards var

--- MemoryGame/MemoryGame/ContentView.swift
+++ MemoryGame/MemoryGame/ContentView.swift
@@ -8,43 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let emojis: [String] = ["👨🏼‍🔬","👨🏼‍⚖️","👨🏼‍💻","🧑🏼‍🏫"]
+    @State var Cardcount = 4
     var body: some View {
-        HStack{
-            CardView(IsFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
-        }
-        .foregroundStyle(.tint)
-        .padding()
-    }
-}
-struct CardView: View{
-    @State var IsFaceUp:Bool = false
-    let texto = "Just works"
-    var body: some View{
-        
-        let base  = RoundedRectangle(cornerRadius: 12)
-        ZStack {
-            if IsFaceUp{
-                base.foregroundColor(.white)
-                base.strokeBorder(lineWidth: 2)
-                VStack{
-                    Text("👨🏼‍💻").font(.largeTitle)
-                    Text(texto).font(.caption)
-                }
-                
-            }else{
-                base.fill()
+        VStack{
+            ScrollView{
+                Cards
             }
-        }.onTapGesture {
-            IsFaceUp.toggle()
-        }
+            
+            Spacer()
+            CardsAdjusters
+            
+        }.padding()
         
     }
+    
+    
+    var Cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<Cardcount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }.aspectRatio(2/3,contentMode: .fill)
+            .foregroundColor(.black)
+        }
+    }
+    
+    
+    var CardsAdjusters: some View{
+        HStack{
+            cardRemover
+            
+            Spacer()
+            
+            CardAdd
+        }
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol:String) -> some View{
+        Button(action: {
+            Cardcount += offset
+        },label: {
+            Image(systemName: symbol)
+        })
+        .disabled(Cardcount + offset < 1 || Cardcount + offset > emojis.count)
+        
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var CardAdd: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    struct CardView: View{
+        @State var IsFaceUp:Bool = false
+        let texto = "Just works"
+        
+        let content: String
+        var body: some View{
+            
+            let base  = RoundedRectangle(cornerRadius: 12)
+            ZStack {
+                Group{
+                    base.foregroundColor(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    VStack{
+                        Text(content).font(.largeTitle)
+                        Text(texto).font(.caption)
+                    }
+                    
+                }
+                .opacity(IsFaceUp ? 1 : 0)
+                base.fill().opacity(IsFaceUp ? 0 : 1)
+            }.onTapGesture {
+                IsFaceUp.toggle()
+            }
+            
+        }
+    }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
+    
+
